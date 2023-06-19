44c8e82f4b0e4136b02dd62884f9be930d39e9fd
Divyanshu soni <amans197555@gmail.com>
2023-06-19 23:47:20 +0530

    added : db conf

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -86,9 +86,17 @@ WSGI_APPLICATION = "ToDoApp.wsgi.app"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'TBolRrSZU9W8',
+        'HOST': 'ep-yellow-bush-353536-pooler.ap-southeast-1.postgres.vercel-storage.com',
+        'PORT': '5432',
     }
 }
 
